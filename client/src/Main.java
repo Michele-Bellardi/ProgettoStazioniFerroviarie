37b402b726e743d8203739ad5249508456478778
michele.bellardi <michele.bellardi2@gmail.com>
2025-04-08 13:26:04 +0200

    creazione progetto client

--- client/src/Main.java
+++ client/src/Main.java
@@ -1,15 +1,35 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public class ClientMaiuscolo {
+        final static String nomeServer = "localhost";
+        final static int portaServer = 4000;
+        public static void main(String[] args) {
+            System.out.println("Connessione al server in corso...");
+            try (Socket sck = new Socket(nomeServer, portaServer)) {
+                String rem = sck.getRemoteSocketAddress().toString();
+                String loc = sck.getLocalSocketAddress().toString();
+                System.out.format("Server (remoto): %s%n", rem);
+                System.out.format("Client (client): %s%n", loc);
+                comunica(sck);
+            } catch (UnknownHostException e) {
+                System.err.format("Nome di server non valido: %s%n", e.getMessage());  } catch (IOException e) {
+                System.err.format("Errore durante la comunicazione con il server: %s%n",  e.getMessage());
+            }
+        }
+        private static void comunica(Socket sck) throws IOException {
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(sck.getInputStream(), "UTF-8"));
+            PrintWriter out = new PrintWriter(
+                    new OutputStreamWriter(sck.getOutputStream(), "UTF-8"), true);
+            Scanner s = new Scanner(System.in, "UTF-8");
+            System.out.print("Frase: ");
+            String frase = s.nextLine();
+            System.out.format("Invio al server: %s%n", frase);
+            out.println(frase);
+            System.out.println("In attesa di risposta dal server...");  String risposta = in.readLine();
+            System.out.format("Risposta dal server: %s%n", risposta);
         }
     }
+
 }
