8fe33dff0b96c6e1f7df6536e34da27ae0798f5a
michele.bellardi <michele.bellardi2@gmail.com>
2025-04-08 13:23:55 +0200

    creazione progetto client

---
+++ server/src/Main.java
@@ -0,0 +1,95 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.File;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Main {
+    public static ArrayList<Stazione> info = new ArrayList<>();
+    final static int PORT = 1050;
+
+    public static void main(String[] args) {
+        prelevaDaFile();
+
+        //TESTING
+        /*
+
+        //stampa prelevato da file
+        for (int i = 0; i < info.size(); i++){
+            System.out.println(info.get(i).toString());
+        }
+
+        Operazioni prove = new Operazioni();
+
+        //riga
+        System.out.println(prove.getRow(3).toString());
+
+        //comune
+        ArrayList<Stazione> stazioniDelComune = prove.getMunicipality("BORGARO TORINESE");
+        for (int i = 0; i < stazioniDelComune.size(); i++){
+            System.out.println(stazioniDelComune.get(i).toString());
+        }
+
+        //nome
+        System.out.println(prove.getName("Matera Centrale").toString());
+
+        //anno
+        ArrayList<Stazione> stazioniDellAnno = prove.getYear("2013");
+        for (int i  = 0; i < stazioniDellAnno.size(); i++){
+            System.out.println(stazioniDellAnno.get(i).toString());
+        }
+
+        //coordinate
+        System.out.println(prove.getCoordinate("8.9750146", "46.0085186").toString());
+
+        //indicatore
+         System.out.println(prove.getIndicator("984003073").toString());
+        */
+
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            Socket clientSocket=null;
+            while(true) {
+                System.out.println("Server Socket: " + serverSocket);
+                clientSocket = serverSocket.accept();
+                Connessione nuovaConnessione = new Connessione(clientSocket);
+                nuovaConnessione.start();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //metodo per prelevare le informazioni presenti nel file csv
+    public static void prelevaDaFile(){
+        String filePath = new File("Mappa-delle-stazioni-ferroviarie-in-Italia.csv").getAbsolutePath();
+        boolean isFirstLine = true;
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (isFirstLine) {
+                    isFirstLine = false;
+                    continue;
+                }
+                //creazione array di informazioni prelevate dal file csv
+                String[] columns = line.split(";");
+                String comune = columns[0];
+                String provincia = columns[1];
+                String regione = columns[2];
+                String nome = columns[3];
+                String annoInserimento = columns[4];
+                String dataEOraInserimento = columns[5];
+                String identificatore = columns[6];
+                String longitudine = columns[7];
+                String latitudine = columns[8];
+
+                //inserimento nell'arraylist
+                info.add(new Stazione(comune, provincia, regione, nome, annoInserimento, dataEOraInserimento, identificatore, longitudine, latitudine));
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ server/src/Opzioni.java
@@ -0,0 +1,27 @@
+public enum Opzioni {
+    GET_ROW ("GET_ROW"),
+    GET_MUNICIPALITY ("GET_MUNICIPALITY"),
+    GET_NAME ("GET_NAME"),
+    GET_YEAR ("GET_YEAR"),
+    GET_COORDINATES ("GET_COORDINATES"),
+    GET_INDICATOR ("GET_INDICATOR");
+
+    private final String nome;
+
+    Opzioni(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public static Opzioni fromString(String text) {
+        for (Opzioni op : Opzioni.values()) {
+            if (op.getNome().equalsIgnoreCase(text)) {
+                return op;
+            }
+        }
+        return null;
+    }
+}
